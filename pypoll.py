db29f0c27953bf249cf3789f7515d899b678ce0f
Sukhjit Zenda <sukhjitzenda1@gmail.com>
2020-03-10 17:21:42 -0700

    .

--- pypoll.py
+++ pypoll.py
@@ -17,6 +17,9 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # 1. Inialize a total vote counter.
 total_votes = 0
 
+# Candidate options.
+candidate_options = []
+
 # Open the elction results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -26,8 +29,21 @@ with open(file_to_load) as election_data:
     
     # Print each row in the CSV file.
     for row in file_reader:
+
         #2. Add to the total vote count
         total_votes += 1
-    
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+    # Print the candidate list.
+    print(candidate_options)
+
     # 3. Print the total votes.
     print(total_votes)
+
